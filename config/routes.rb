a5d9dc9d119b3fc513e228438578a3af0877476c
VidensKnowledge <devydoolander@gmail.com>
2020-09-12 13:49:33 -0600

    double sign up, error in devise views

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,29 @@
 Rails.application.routes.draw do
-  get 'locations/index'
-  get 'locations/show'
-  get 'locations/edit'
-  get 'locations/new'
-  get 'addresses/index'
-  get 'addresses/show'
-  get 'addresses/new'
-  get 'trips/index'
-  get 'trips/show'
-  get 'trips/new'
-  get 'trips/edit'
+  # get 'locations/index'
+  # get 'locations/show'
+  # get 'locations/edit'
+  # get 'locations/new'
+  # get 'addresses/index'
+  # get 'addresses/show'
+  # get 'addresses/new'
+  # get 'trips/index'
+  # get 'trips/show'
+  # get 'trips/new'
+  # get 'trips/edit'
   devise_for :users
+  root "accounts#index"
+
+  resources :trips do
+    resources :locations 
+  end
+    
+  resource :locations do
+    resources :Addresses
+  end
 
   ## Todo add model resources later linking trips, locations, addresses
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
+
+#This is a test from Jon. 
+# scott here
